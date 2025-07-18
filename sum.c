e29bf68d6c021206d26dca2e4a770f951eed593e
ahmad-icp <ahmad.icp.dev@gmail.com>
2025-07-18 20:30:26 +0500

    sum of n number using recursion

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int sumn(int n)
+{ if (n==0)
+return 0;
+int sum=sumn(n-1) +n;
+return sum;
+}
+int main()
+{ int n;
+  printf("Enter a positive integer: ");
+  scanf("%d", &n);
+  printf("Sum of first %d natural numbers is: %d\n", n, sumn(n));
+  return 0;
+}
