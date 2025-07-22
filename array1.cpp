dca6b76225e5e9dbb9e2e2ebd868a4a0bd48a48e
ahmad-icp <ahmad.icp.dev@gmail.com>
2025-07-22 13:15:36 +0500

    operations on array

--- array1.cpp
+++ array1.cpp
@@ -9,7 +9,31 @@ int main(){
        cout << arr[i] << " ";
    }
    cout << endl;
-
+arr.remove(3);
+   cout << "Array after removing 3: ";
+    for(int i = 0; i < 5; i++) {
+         cout << arr[i] << " ";
+    }
+    cout << endl;
+    int searchElement = 4;
+    bool found = false;
+    for(int i = 0; i < 5; i++) {
+        if(arr[i] == searchElement) {
+            found = true;
+            break;
+        }
+    }
+    if(found) {
+        cout << "Element " << searchElement << " found in the array." << endl;
+    } else {
+        cout << "Element " << searchElement << " not found in the array." << endl;
+    }
+    arr.add(2,8);
+    cout << "Array after adding 8 at index 2: ";
+    for(int i = 0; i < 6; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
    return 0;
 }
 
