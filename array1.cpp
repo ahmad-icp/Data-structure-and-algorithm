756f5f288186e3bee19d3eaaa4549fdc6c6deeb7
ahmad-icp <ahmad.icp.dev@gmail.com>
2025-07-22 13:11:57 +0500

    adding new element

---
+++ array1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+   int arr[] = {1, 2, 3, 4, 5};
+   arr.add(7);
+   cout << "Array after adding 7: ";
+   for(int i = 0; i < 6; i++) {
+       cout << arr[i] << " ";
+   }
+   cout << endl;
+
+   return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
