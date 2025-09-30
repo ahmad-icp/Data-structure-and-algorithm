cff1efba8af2ffd4136057947bb0a00fc96850d3
Ahmad-icp <ahmad.icp.dev@gmail.com>
2025-09-30 19:20:45 +0500

    fixing issues

--- array1.cpp
+++ array1.cpp
@@ -1,58 +1,63 @@
 #include <iostream>
 using namespace std;
-int main(){
 
-   int arr[] = {1, 2, 3, 4, 5};
-   arr.add(7);
-   cout << "Array after adding 7: ";
-   for(int i = 0; i < 6; i++) {
-       cout << arr[i] << " ";
-   }
-   cout << endl;
-arr.remove(3);
-   cout << "Array after removing 3: ";
-    for(int i = 0; i < 5; i++) {
-         cout << arr[i] << " ";
+int main() {
+    int arr[10] = {1, 2, 3, 4, 5}; // capacity = 10
+    int size = 5; // current number of elements
+
+    // Add 7 at the end
+    arr[size] = 7;
+    size++;
+    cout << "Array after adding 7: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
     }
     cout << endl;
+
+    // Remove element with value 3
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == 3) {
+            for (int j = i; j < size - 1; j++) {
+                arr[j] = arr[j + 1]; // shift left
+            }
+            size--;
+            break;
+        }
+    }
+    cout << "Array after removing 3: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    // Search for element
     int searchElement = 4;
     bool found = false;
-    for(int i = 0; i < 5; i++) {
-        if(arr[i] == searchElement) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == searchElement) {
             found = true;
             break;
         }
     }
-    if(found) {
+    if (found) {
         cout << "Element " << searchElement << " found in the array." << endl;
     } else {
         cout << "Element " << searchElement << " not found in the array." << endl;
     }
-    arr.add(2,8);
+
+    // Insert 8 at index 2
+    int index = 2;
+    for (int i = size; i > index; i--) {
+        arr[i] = arr[i - 1]; // shift right
+    }
+    arr[index] = 8;
+    size++;
+
     cout << "Array after adding 8 at index 2: ";
-    for(int i = 0; i < 6; i++) {
-        cout << arr[i] << " ";  
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
     }
     cout << endl;
-   return 0;
+
+    return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
