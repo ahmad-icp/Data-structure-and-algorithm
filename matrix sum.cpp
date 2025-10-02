fdf7e11e4463b6aad27527a2fc543f32dcbb76dc
Ahmad-icp <ahmad.icp.dev@gmail.com>
2025-10-02 20:09:32 +0500

    matrix addition

--- matrix sum.cpp
+++ matrix sum.cpp
@@ -1,41 +1,62 @@
 #include<iostream>
 using namespace std;
 int main()
-{int m,n;
+{
+    int m,n;
     cout <<"enter number of rows";
     cin>>m;
     cout<<"enter number of columns";
     cin>>n;
-    int arr[m][n]={{}};
+
+    int arr[m][n];   
+
     for (int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             cin>>arr[i][j];
         }
     }
+
     for (int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             cout<<arr[i][j]<<" ";
         }
         cout<<endl;
     }
-int a,b;
+
+    int a,b;
     cout <<"enter number of rows";
     cin>>a;
     cout<<"enter number of columns";
     cin>>b;
-    int arr1[a][b]={{}};
+
+    int arr1[a][b];  
+
     for (int i=0;i<a;i++){
         for(int j=0;j<b;j++){
             cin>>arr1[i][j];
         }
     }
+
     for (int i=0;i<a;i++){
         for(int j=0;j<b;j++){
             cout<<arr1[i][j]<<" ";
         }
         cout<<endl;
-
     }
-int sumOfArrays[i][j]=arr[i][j]+arr1[i][j];
-        cout<<sumOfArrays[i][j]<<" ";
-}  
+
+    
+    if (m==a && n==b) {
+        int sumOfArrays[m][n];
+        for (int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                sumOfArrays[i][j] = arr[i][j] + arr1[i][j];
+                cout<<sumOfArrays[i][j]<<" ";
+            }
+            
+        }
+    } else {
+        cout << "Matrix dimensions must match for addition!" << endl;
+    }
+
+    return 0;
+}
